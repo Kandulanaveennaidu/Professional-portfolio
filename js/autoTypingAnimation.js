ef0e311439b394f0f2518635692c3509901c98cb
Kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-12-23 13:00:55 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Kandula Naveen</span>  a dedicated professional passionate about leveraging technology to create impactful solutions. With a keen eye for detail and a commitment to innovation, I specialize in crafting seamless experiences and robust digital strategies. My goal is to deliver exceptional results while adapting to evolving challenges."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
